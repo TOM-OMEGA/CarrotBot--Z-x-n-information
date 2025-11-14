5c29507e66e3243d1b16b7cb930d13a45539e062
TOM-OMEGA <aliensvsgdi@gmail.com>
2025-11-14 09:20:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import disnake
+from disnake.ext import commands
 import os
 from dotenv import load_dotenv
 from keepalive import keep_alive
@@ -8,7 +8,8 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-intents = discord.Intents.default()
+# 禁用 VOICE，避免 audioop
+intents = disnake.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -21,8 +22,7 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
-# 重要：啟動 Flask 保活
+# Render 保活
 keep_alive()
 
-# 啟動 BOT
 bot.run(TOKEN)
